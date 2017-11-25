187361745d8ac28acba9f78ca00475b0c10de8cc
o002073 <Ryan.J.Newsom@kp.org>
2017-11-25 13:40:11 -0700

    refactored to make index.js a class component instead of a functional one

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAwg7pjsLVrgON6_9Pq3stwCM86xDAlZIQ';
+const DEFAULT_SEARCH = 'surfboards'
 
-//Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: DEFAULT_SEARCH}, (videos) => {
+            this.setState({ videos }); //es6 ftw
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
-//Take this component's generated HTML and put it on the page(in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
