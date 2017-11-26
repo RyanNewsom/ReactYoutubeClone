3e2e97c80034c49df1a08903a092949010de9e4e
o002073 <Ryan.J.Newsom@kp.org>
2017-11-26 13:06:46 -0700

    search now works

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,16 +11,18 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input 
                 value={this.state.term}
-                onChange={event => this.setState({ term: event.target.value })} 
+                onChange={event => this.onInputChange(event.target.value)} 
                 />
             </div>
         );
     }
 
-    onInputChange(event) {
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChanged(term);
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash'
 
 import VideoList from './components/video_list'
 import SearchBar from './components/search_bar';
@@ -19,18 +20,26 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: DEFAULT_SEARCH}, (videos) => {
+        this.videoSearch(DEFAULT_SEARCH)
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
             this.setState({ 
                 videos: videos,
                 selectedVideo: videos[0]
-            }); //es6 ftw
+            });
         });
     }
 
     render() {
+        const videoSearch = _.debounce((term) => {
+            this.videoSearch(term)
+        }, 300);
+
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChanged={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
